562c418e5925e59438761fe81292e1d30db6f669
djlevinson98 <140760896+djlevinson98@users.noreply.github.com>
2024-10-15 11:49:35 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,65 @@
+public class Main{
+    public static void main(String[] args){
 
+        System.out.println("\nProblem 1: Name Initials");
+        /*
+         *  Problem 1: Name Initials
+         *  a. Declare and initialize a String variable with a first and last name separated by a space " " character (e.g. “John Smith”).
+         *  b. Use String methods to extract the first initial and last initial (e.g. “JS”).
+         *  c. Print a statement with the initials in the following format:
+         *      John Smith’s initials are JS.
+         *  d. Your code should be able to work for any first and last name. You should not need to change any code if you change the name to something longer or shorter. 
+         */
+        
+
+         
+
+         
+
+
+         /*
+         *  Problem 2: NumberGames
+         *  Using the NumberGames methods, you will need to write code to accomplish the following:
+         *  a. Square the number and print it out. 
+         *  b. Double the result and print it out.
+         *  c. Square the result and print it out. 
+         *  d. Get the result and store it in a variable, then print out the number. 
+         *  e. After finishing your code, there is room at the bottom of the page for you to write what the output would be after running the code. 
+         *  YOU MUST USE ALL METHODS FROM THE NUMBERGAMES CLASS
+         */
+        System.out.println("\nProblem 2: NumberGames");
+        NumberGames game = new NumberGames(3.0);
+
+
+
+
+
+        /*
+         *  Problem 3: RaceCar
+         *  a. In the Racecar class, add code to complete the ComputeTime() method.
+         *  b. Generate a random acceleration for each car on the interval [2,5).
+         *  c. Create two Racecar objects.
+         *  d. Compute the finishing times for both cars.
+         *  e. Print times of each car.
+         */
+        System.out.println("\nProblem 3: RaceCar");
+        
+        // Length of the course in meters
+        double distance = 2414;
+
+        // Generate a random acceleration for each car on the interval [2,5)
+
+
+        // Create two Racecar objects
+
+
+        // Compute the finishing times for both cars
+
+
+        // Print times of each car
+
+
+
+        System.out.println("\n");
+    }
+}
